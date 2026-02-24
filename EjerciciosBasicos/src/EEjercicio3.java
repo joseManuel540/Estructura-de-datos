bda935c747821beabd4e552b007c955d5bbd2edf
joseManuel540 <josenoob312@gmail.com>
2026-02-24 07:07:18 -0500

    cambios

--- EjerciciosBasicos/src/EEjercicio3.java
+++ EjerciciosBasicos/src/EEjercicio3.java
@@ -9,20 +9,23 @@ llamada enAlarma debe ser true */
 public class EEjercicio3 {
     public static void main(String[] args) {
         
-        double TempAct = 40; //en celcius
+        double TempAct = 90; //en celcius
         String NombreSen = "Alarma";
         int NumLectura = 2;
-        boolean enAlarma = false;
+        boolean enAlarma;
 
         if (TempAct>80) {
             enAlarma = true;
         }
+        else {
+            enAlarma = false;
+        }
 
-        System.out.println("=== Estado del sensor ===");
+        System.out.println("====== Estado del sensor ======");
         System.out.println("temperatura actual: "+ TempAct);
         System.out.println("nombre: "+ NombreSen);
         System.out.println("numero lectura: "+ NumLectura);
-        System.out.println("Alarma activa: "+ enAlarma);
+        System.out.println("Alarma activada: "+ enAlarma);
         
     }
 }
