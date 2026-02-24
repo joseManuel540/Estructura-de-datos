aa008a09d34206bc51432365ee2e4787cf07544c
joseManuel540 <josenoob312@gmail.com>
2026-02-24 07:01:02 -0500

    comentarios

--- EjerciciosBasicos/src/EEjercicio8.java
+++ EjerciciosBasicos/src/EEjercicio8.java
@@ -1,12 +1,13 @@
+/*Ejercicio 8 — Historial de mensajes
+Un chat guarda el último mensaje enviado. Se usan dos variables String:
+ultimoMensaje = "Hola"
+copiaTexto = ultimoMensaje
+Luego copiaTexto se cambia a "¿Cómo estás?"
+Muestra ambas variables y explica en un comentario por qué ultimoMensaje
+no cambió (inmutabilidad de String). */
+
 public class EEjercicio8 {
     public static void main(String[] args) {  
-        /*Ejercicio 8 — Historial de mensajes
-        Un chat guarda el último mensaje enviado. Se usan dos variables String:
-        ultimoMensaje = "Hola"
-        copiaTexto = ultimoMensaje
-        Luego copiaTexto se cambia a "¿Cómo estás?"
-        Muestra ambas variables y explica en un comentario por qué ultimoMensaje
-        no cambió (inmutabilidad de String). */
 
         String ultimoMensaje = "Hola";
         String copiaTexto = ultimoMensaje;
